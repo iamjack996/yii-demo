2ca57b4df2738fba07e7472d62c1572599e8d068
User <iamjack996@gmail.com>
2021-03-25 11:35:23 +0800

    0325 first commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=178.128.88.251;dbname=demo',
+    'username' => 'jack',
+    'password' => '3ZnZmytKjtdRyM2a',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
